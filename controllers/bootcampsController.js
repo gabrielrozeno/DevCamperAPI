d3c49360f8f1af03c7370e5e1c68c691beb7422c
Gabriel Rozeno <gabrozenodev@gmail.com>
2023-10-25 16:53:39 -0300

    created basic version of getBootcamps and getBootcampById method

--- controllers/bootcampsController.js
+++ controllers/bootcampsController.js
@@ -3,15 +3,42 @@ const Bootcamp = require("../models/Bootcamp");
 // @desc    Busca Todos os Bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  PUBLIC
-exports.getBootcamps = (req, res, next) => {
-  res.json({ success: true, msg: "Busca todos os bootcamps" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({
+      success: true,
+      data: bootcamps,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc    Busca os Bootcamps Por ID
 // @route   GET /api/v1/bootcamps/:id
 // @access  PUBLIC
-exports.getBootcampById = (req, res, next) => {
-  res.json({ success: true, msg: `Busca bootcamp por ID: ${req.params.id}` });
+exports.getBootcampById = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(404).json({
+        success: false,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @desc    Cria um Bootcamp
